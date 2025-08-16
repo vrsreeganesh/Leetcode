ff0d518d91d1e915b7c461e3da335c4d8a7dc448
vrsreeganesh <vrsreeganesh@gmail.com>
2025-08-16 10:02:55 -0400

    Addition: 189. Rotate Array

---
+++ Code/RotateArray.cpp
@@ -0,0 +1,174 @@
+// including header-files
+#include <algorithm>
+#include <bitset>
+#include <climits>
+#include <cstddef>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <new>
+#include <stdlib.h>
+#include <unordered_map>
+#include <vector>
+#include <set>
+
+// hash-deinfes
+#define PRINTSPACE  std::cout << "\n\n\n\n" << std::endl;
+#define PRINTLINE   std::cout << "==============================================" << std::endl;
+
+// borrowing from namespace std
+using std::cout;
+using std::endl;
+using std::vector;
+using std::string;
+using std::unordered_map;
+using std::map;
+using std::format;
+using std::deque;
+using std::pair;
+
+// vector printing function
+template<typename T>
+void fPrintVector(vector<T> input){
+    for(auto x: input) cout << x << ",";
+    cout << endl;
+}
+
+template<typename T>
+void fpv(vector<T> input){
+    for(auto x: input) cout << x << ",";
+    cout << endl;
+}
+
+template<typename T>
+void fPrintMatrix(vector<T> input){
+    for(auto x: input){
+        for(auto y: x){
+            cout << y << ",";
+        }
+        cout << endl;
+    }
+}
+
+template<typename T, typename T1>
+void fPrintHashmap(unordered_map<T, T1> input){
+    for(auto x: input){
+        cout << format("[{},{}] | ", x.first, x.second);
+    }
+    cout <<endl;
+}
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+void fPrintBinaryTree(TreeNode* root){
+    // sending it back
+    if (root == nullptr) return;
+    
+    // printing
+    PRINTLINE
+    cout << "root->val = " << root->val << endl;
+    
+    // calling the children
+    fPrintBinaryTree(root->left);
+    fPrintBinaryTree(root->right);
+    
+    // returning
+    return;
+    
+}
+
+void fPrintLinkedList(ListNode* root){
+    if (root == nullptr) return;
+    cout << root->val << ", ";
+    fPrintLinkedList(root);
+    return;
+}
+
+template<typename T>
+void fPrintContainer(T input){
+    for(auto x: input) cout << x << ", ";
+    cout << endl;
+    return;
+}
+
+struct StopWatch
+{
+    std::chrono::time_point<std::chrono::high_resolution_clock> startpoint;
+    std::chrono::time_point<std::chrono::high_resolution_clock> endpoint;
+    std::chrono::duration<long long, std::nano>                 duration;
+    
+    // constructor
+    StopWatch()     {startpoint = std::chrono::high_resolution_clock::now();}
+    void start()    {startpoint = std::chrono::high_resolution_clock::now();}
+    void stop()     {endpoint   = std::chrono::high_resolution_clock::now(); fetchtime();}
+    
+    void fetchtime(){
+        duration = std::chrono::duration_cast<std::chrono::nanoseconds>(endpoint - startpoint);
+        cout << format("{}  nanoseconds \n", duration.count());
+    }
+    void fetchtime(string stringarg){
+        duration = std::chrono::duration_cast<std::chrono::nanoseconds>(endpoint - startpoint);
+        cout << format("{} took {}  nanoseconds \n", stringarg, duration.count());
+    }
+};
+
+
+// main-file ===================================================================
+int main(){
+    
+    // input- configuration
+    vector<int> nums    {-1,-100,3,99};
+    int k   {2};
+
+    // setup
+    k = k %static_cast<int>(nums.size());                   // to ensure that the value is within range
+
+    int source          {0};
+    int temp_source     {nums[source]};
+    int temp            {0};
+    int destination     {0};
+
+    vector<bool> sourcelist(nums.size(), false);
+
+    // going through nums
+    for(int i = 0; i < nums.size(); ++i){
+
+        // check if curent-source has been taken care of
+        if (sourcelist[source] == true){
+            source      = (source+1) % nums.size(); 
+            temp_source = nums[source];
+        }
+
+        source              = source % nums.size();         // code to ensure range
+        destination         = (source + k)%nums.size();     // calculating the index we'll be writing to 
+        sourcelist[source]  = true;                         // updating source-list
+
+        temp                = nums[destination];            // safe-keeping the destination value
+        nums[destination]   = temp_source;                  // storing new value at destination-index
+
+        source              = destination;                  // updating source-index
+        temp_source         = temp;                         // updating source-value
+    }
+
+    // printing the output
+    cout << format("nums = "); fpv(nums);
+    
+    // return
+    return(0);    
+}
