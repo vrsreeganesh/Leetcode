47952ccefeb8f52d538088b0a8942cab39df0959
vrsreeganesh <vrsreeganesh@gmail.com>
2025-08-17 20:04:23 -0400

    Added: 3 Longest Substring Without Repeating Characters

---
+++ Code/LongestSubstringWithoutRepeatingCharacters.cpp
@@ -0,0 +1,178 @@
+// including header-files
+#include <bitset>
+#include <climits>
+#include <cstddef>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <new>
+#include <stdlib.h>
+#include <unordered_map>
+#include <vector>
+#include <set>
+
+// hash-deinfes
+#define PRINTSPACE  std::cout << "\n\n\n\n" << std::endl;
+#define PRINTLINE   std::cout << "-------------------------------" << std::endl;
+
+// borrowing from namespace std
+using std::cout;
+using std::endl;
+using std::vector;
+using std::string;
+using std::unordered_map;
+using std::map;
+using std::format;
+using std::deque;
+using std::pair;
+
+// vector printing function
+template<typename T>
+void fPrintVector(vector<T> input){
+    for(auto x: input) cout << x << ",";
+    cout << endl;
+}
+
+template<typename T>
+void fPrintMatrix(vector<T> input){
+    for(auto x: input){
+        for(auto y: x){
+            cout << y << ",";
+        }
+        cout << endl;
+    }
+}
+
+template<typename T, typename T1>
+void fPrintHashmap(unordered_map<T, T1> input){
+    for(auto x: input){
+        cout << format("[{},{}] ", x.first, x.second);
+    }
+    cout <<endl;
+}
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+void fPrintBinaryTree(TreeNode* root){
+    // sending it back
+    if (root == nullptr) return;
+    
+    // printing
+    PRINTLINE
+    cout << "root->val = " << root->val << endl;
+    
+    // calling the children
+    fPrintBinaryTree(root->left);
+    fPrintBinaryTree(root->right);
+    
+    // returning
+    return;
+    
+}
+
+void fPrintLinkedList(ListNode* root){
+    if (root == nullptr) return;
+    cout << root->val << ", ";
+    fPrintLinkedList(root);
+    return;
+}
+
+template<typename T>
+void fPrintContainer(T input){
+    for(auto x: input) cout << x << ", ";
+    cout << endl;
+    return;
+}
+
+struct StopWatch
+{
+    std::chrono::time_point<std::chrono::high_resolution_clock> startpoint;
+    std::chrono::time_point<std::chrono::high_resolution_clock> endpoint;
+    std::chrono::duration<long long, std::nano>                 duration;
+    
+    // constructor
+    StopWatch()     {startpoint = std::chrono::high_resolution_clock::now();}
+    void start()    {startpoint = std::chrono::high_resolution_clock::now();}
+    void stop()     {endpoint   = std::chrono::high_resolution_clock::now(); fetchtime();}
+    
+    void fetchtime(){
+        duration = std::chrono::duration_cast<std::chrono::nanoseconds>(endpoint - startpoint);
+        cout << format("{}  nanoseconds \n", duration.count());
+    }
+    void fetchtime(string stringarg){
+        duration = std::chrono::duration_cast<std::chrono::nanoseconds>(endpoint - startpoint);
+        cout << format("{} took {}  nanoseconds \n", stringarg, duration.count());
+    }
+};
+
+
+// main-file ===================================================================
+int main(){
+    
+    // input- configuration
+    string s    {"tmmzuxt"};
+
+    // setup
+    unordered_map<char, int> histogram;
+    int p1  {0};
+    char curr;
+    int finaloutput {-1};
+    int temp_length {-1};
+
+    // going through the thing 
+    for(int p2 = 0; p2<s.size(); ++p2){
+
+        // moving to another variable
+        curr = s[p2];
+
+        // checking if current character is in histogram 
+        if (histogram.find(curr) == histogram.end()) [[unlikely]]
+        {
+            histogram[curr] = 1;
+        }
+        else [[likely]]
+        {
+            // checking if count is zero
+            if (histogram[curr] == 0)
+            {
+                histogram[curr] = 1;
+            }
+            else
+            {
+                // moving p1 until it arrives at first instance of curr
+                while(s[p1] != curr)    
+                {
+                    --histogram[s[p1]];
+                    ++p1;
+                }
+                ++p1;
+                histogram[curr] = 1; 
+            }
+        }
+
+        // calculating longest length
+        finaloutput = finaloutput > (p2-p1+1) ? finaloutput : (p2-p1+1);
+    }
+
+    // printing 
+    cout << format("longest length = {} \n", finaloutput);
+
+    // return
+    return(0);    
+}
