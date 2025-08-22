fa6c577bd1e8b9cca5d1bfadd37f80e4e62a2719
vrsreeganesh <vrsreeganesh@gmail.com>
2025-08-21 22:21:47 -0400

    Added: 443. String Compression

---
+++ Code/StringCompression.cpp
@@ -0,0 +1,179 @@
+// including header-files
+#include <bitset>
+#include <climits>
+#include <cstddef>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <new>
+#include <stdlib.h>
+#include <unordered_map>
+#include <vector>
+#include <set>
+
+// hash-deinfes
+#define PRINTSPACE  std::cout << "\n\n\n\n" << std::endl;
+#define PRINTLINE   std::cout << "-----------------" << std::endl;
+
+// borrowing from namespace std
+using std::cout;
+using std::endl;
+using std::vector;
+using std::string;
+using std::unordered_map;
+using std::map;
+using std::format;
+using std::deque;
+using std::pair;
+
+// vector printing function
+template<typename T>
+void fPrintVector(vector<T> input){
+    for(auto x: input) cout << x << ",";
+    cout << endl;
+}
+
+template<typename T>
+void fPrintMatrix(vector<T> input){
+    for(auto x: input){
+        for(auto y: x){
+            cout << y << ",";
+        }
+        cout << endl;
+    }
+}
+
+template<typename T, typename T1>
+void fPrintHashmap(unordered_map<T, T1> input){
+    for(auto x: input){
+        cout << format("[{},{}] \n", x.first, x.second);
+    }
+    cout <<endl;
+}
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+void fPrintBinaryTree(TreeNode* root){
+    // sending it back
+    if (root == nullptr) return;
+    
+    // printing
+    PRINTLINE
+    cout << "root->val = " << root->val << endl;
+    
+    // calling the children
+    fPrintBinaryTree(root->left);
+    fPrintBinaryTree(root->right);
+    
+    // returning
+    return;
+    
+}
+
+void fPrintLinkedList(ListNode* root){
+    if (root == nullptr) return;
+    cout << root->val << ", ";
+    fPrintLinkedList(root);
+    return;
+}
+
+template<typename T>
+void fPrintContainer(T input){
+    for(auto x: input) cout << x << ", ";
+    cout << endl;
+    return;
+}
+
+struct StopWatch
+{
+    std::chrono::time_point<std::chrono::high_resolution_clock> startpoint;
+    std::chrono::time_point<std::chrono::high_resolution_clock> endpoint;
+    std::chrono::duration<long long, std::nano>                 duration;
+    
+    // constructor
+    StopWatch()     {startpoint = std::chrono::high_resolution_clock::now();}
+    void start()    {startpoint = std::chrono::high_resolution_clock::now();}
+    void stop()     {endpoint   = std::chrono::high_resolution_clock::now(); fetchtime();}
+    
+    void fetchtime(){
+        duration = std::chrono::duration_cast<std::chrono::nanoseconds>(endpoint - startpoint);
+        cout << format("{}  nanoseconds \n", duration.count());
+    }
+    void fetchtime(string stringarg){
+        duration = std::chrono::duration_cast<std::chrono::nanoseconds>(endpoint - startpoint);
+        cout << format("{} took {}  nanoseconds \n", stringarg, duration.count());
+    }
+};
+
+
+// main-file ===================================================================
+int main(){
+    
+    // input- configuration
+    vector<char> chars  {'a','b','b','b','b','b','b','b','b','b','b','b','b'};
+    
+    // going through the character
+    int p1              {0};
+    char runningchar    {};
+    char curr           {};
+    int count           {0};
+    string finaloutput;
+
+    // going through the inputs
+    while(p1<chars.size()){
+
+        // getting curren tchar
+        curr = chars[p1];
+
+        // increasing count 
+        if (count == 0)                 {runningchar = chars[p1]; ++count;}
+        else if(curr == runningchar)    {++count;}
+        else if(curr != runningchar)    {
+            finaloutput += runningchar; // writing character to current pointer
+            if (count != 1) 
+                finaloutput += std::to_string(count); // increasing write-pointer
+            runningchar = curr;
+            count = 1;
+        }
+
+        // increasing pointer
+        ++p1;
+    }
+
+    // flushing out 
+    if (count != 0){
+        finaloutput += runningchar; // writing character to current pointer
+        if (count != 1) 
+            finaloutput += std::to_string(count); // increasing write-pointer
+    }
+
+    // writing to input
+    for(int i = 0; i<finaloutput.size(); ++i){
+        chars[i] = finaloutput[i];
+    }
+
+    // printing the final output
+    cout << format("finaloutput = {}\n", finaloutput);
+    cout << "chars = "; fPrintVector(chars);
+    cout << format("return-value = {}\n", finaloutput.size());
+    
+    // return
+    return(0);
+    
+}
