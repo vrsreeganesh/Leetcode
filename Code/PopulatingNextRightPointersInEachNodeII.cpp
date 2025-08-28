63a6c56d4fa86fbfdf3d39384f62110fec446663
vrsreeganesh <vrsreeganesh@gmail.com>
2025-08-27 23:05:54 -0400

    Added: 117. Populating Next Right Pointers in Each Node II

---
+++ Code/PopulatingNextRightPointersInEachNodeII.cpp
@@ -0,0 +1,207 @@
+// including header-files
+#include <algorithm>
+#include <bitset>
+#include <climits>
+#include <cstddef>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <new>
+#include <stdlib.h>
+#include <unordered_map>
+#include <vector>
+#include <set>
+#include <numeric>
+#include <functional>
+
+
+// hash-deinfes
+#define PRINTSPACE  std::cout << "\n\n\n\n" << std::endl;
+#define PRINTLINE   std::cout << "==============================================" << std::endl;
+
+// borrowing from namespace std
+using std::cout;
+using std::endl;
+using std::vector;
+using std::string;
+using std::unordered_map;
+using std::map;
+using std::format;
+using std::deque;
+using std::pair;
+using std::min;
+using std::max;
+
+// vector printing function
+template<typename T>
+void fPrintVector(vector<T> input){
+    for(auto x: input) cout << x << ",";
+    cout << endl;
+}
+
+template<typename T>
+void fpv(vector<T> input){
+    for(auto x: input) cout << x << ",";
+    cout << endl;
+}
+
+template<typename T>
+void fPrintMatrix(vector<T> input){
+    for(auto x: input){
+        for(auto y: x){
+            cout << y << ",";
+        }
+        cout << endl;
+    }
+}
+
+template<typename T, typename T1>
+void fPrintHashmap(unordered_map<T, T1> input){
+    for(auto x: input){
+        cout << format("[{},{}] | ", x.first, x.second);
+    }
+    cout <<endl;
+}
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+void fPrintBinaryTree(TreeNode* root){
+    // sending it back
+    if (root == nullptr) return;
+    
+    // printing
+    PRINTLINE
+    cout << "root->val = " << root->val << endl;
+    
+    // calling the children
+    fPrintBinaryTree(root->left);
+    fPrintBinaryTree(root->right);
+    
+    // returning
+    return;
+    
+}
+
+void fPrintLinkedList(string    prefix,
+                      ListNode* root){
+    if (root == nullptr) return;
+    cout << prefix;
+    std::function<void(ListNode*)> runlinkedlist = [&runlinkedlist](ListNode* root){
+        if (root == nullptr) return;
+        cout << root->val << " -> ";
+        runlinkedlist(root->next);
+    };
+    runlinkedlist(root);
+    cout << "|" << endl;
+    return;
+}
+
+template<typename T>
+void fPrintContainer(T input){
+    for(auto x: input) cout << x << ", ";
+    cout << endl;
+    return;
+}
+
+struct Timer
+{
+    std::chrono::time_point<std::chrono::high_resolution_clock> startpoint;
+    std::chrono::time_point<std::chrono::high_resolution_clock> endpoint;
+    std::chrono::duration<long long, std::nano>                 duration;
+    
+    // constructor
+    Timer()         {startpoint = std::chrono::high_resolution_clock::now();}
+    void start()    {startpoint = std::chrono::high_resolution_clock::now();}
+    void stop()     {endpoint   = std::chrono::high_resolution_clock::now(); fetchtime();}
+    
+    void fetchtime(){
+        duration = std::chrono::duration_cast<std::chrono::nanoseconds>(endpoint - startpoint);
+        cout << format("{}  nanoseconds \n", duration.count());
+    }
+    void fetchtime(string stringarg){
+        duration = std::chrono::duration_cast<std::chrono::nanoseconds>(endpoint - startpoint);
+        cout << format("{} took {}  nanoseconds \n", stringarg, duration.count());
+    }
+    void measure(){
+        auto temp = std::chrono::high_resolution_clock::now();
+        auto nsduration  = std::chrono::duration_cast<std::chrono::nanoseconds>(temp - startpoint);
+        auto msduration  = std::chrono::duration_cast<std::chrono::microseconds>(temp - startpoint);
+        auto sduration   = std::chrono::duration_cast<std::chrono::seconds>(temp - startpoint);
+        cout << format("{} nanoseconds | {} microseconds | {} seconds \n", 
+            nsduration.count(), msduration.count(), sduration.count());
+    }
+    ~Timer(){
+        measure();
+    }
+};
+
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+
+
+int main(){
+
+    // starting timer
+    Timer timer;
+    
+    // input- configuration
+    auto root           {new Node(1)};
+    root->left          = new Node(2);
+    root->right         = new Node(3);
+    root->left->left    = new Node(4);
+    root->left->right   = new Node(5);
+    root->right->right  = new Node(7);
+
+    // running
+    if (root == nullptr) {cout << format("done!\n"); return 0;}
+
+    // calling the function
+    int count = -1;
+    vector< vector<Node*> > addressvectors;
+
+    // calling th efunction
+    foo(root, count, addressvectors);
+
+    // building the connections
+    for(auto x: addressvectors){
+        int i = 0;
+        for(; i<x.size()-1; ++i)
+            if (x[i] != nullptr)    {x[i]->next = x[i+1];}                      // look to the right 
+
+        // ensuring that the last pointer points to thr right most 
+        x[i]->next = nullptr;
+    }
+
+    // completion
+    cout << format("done!\n"); 
+
+    // return
+    return(0);
+    
+}
