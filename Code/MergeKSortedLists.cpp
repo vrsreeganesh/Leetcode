c8745c36ec0495704d080a55876a2b5d654960c2
vrsreeganesh <vrsreeganesh@gmail.com>
2025-08-30 05:43:00 -0400

    Added: 23. Merge k Sorted Lists

---
+++ Code/MergeKSortedLists.cpp
@@ -0,0 +1,217 @@
+// including header-files
+#include <algorithm>
+#include <unordered_set>
+#include <bitset>
+#include <climits>
+#include <cstddef>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <new>
+#include <stdlib.h>
+#include <unordered_map>
+#include <vector>
+#include <set>
+#include <numeric>
+#include <functional>
+
+
+// hash-deinfes
+#define PRINTSPACE  std::cout << "\n\n\n\n" << std::endl;
+#define PRINTLINE   std::cout << "==============================================" << std::endl;
+
+// borrowing from namespace std
+using std::cout;
+using std::endl;
+using std::vector;
+using std::string;
+using std::unordered_map;
+using std::map;
+using std::format;
+using std::deque;
+using std::pair;
+using std::min;
+using std::max;
+
+// vector printing function
+template<typename T>
+void fPrintVector(vector<T> input){
+    for(auto x: input) cout << x << ",";
+    cout << endl;
+}
+
+template<typename T>
+void fpv(vector<T> input){
+    for(auto x: input) cout << x << ",";
+    cout << endl;
+}
+
+template<typename T>
+void fPrintMatrix(vector<T> input){
+    for(auto x: input){
+        for(auto y: x){
+            cout << y << ",";
+        }
+        cout << endl;
+    }
+}
+
+template<typename T, typename T1>
+void fPrintHashmap(unordered_map<T, T1> input){
+    for(auto x: input){
+        cout << format("[{},{}] | ", x.first, x.second);
+    }
+    cout <<endl;
+}
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+void fPrintBinaryTree(TreeNode* root){
+    // sending it back
+    if (root == nullptr) return;
+    
+    // printing
+    PRINTLINE
+    cout << "root->val = " << root->val << endl;
+    
+    // calling the children
+    fPrintBinaryTree(root->left);
+    fPrintBinaryTree(root->right);
+    
+    // returning
+    return;
+    
+}
+
+void fPrintLinkedList(string    prefix,
+                      ListNode* root){
+    if (root == nullptr) return;
+    cout << prefix;
+    std::function<void(ListNode*)> runlinkedlist = [&runlinkedlist](ListNode* root){
+        if (root == nullptr) return;
+        cout << root->val << " -> ";
+        runlinkedlist(root->next);
+    };
+    runlinkedlist(root);
+    cout << "|" << endl;
+    return;
+}
+
+template<typename T>
+void fPrintContainer(T input){
+    for(auto x: input) cout << x << ", ";
+    cout << endl;
+    return;
+}
+
+struct Timer
+{
+    std::chrono::time_point<std::chrono::high_resolution_clock> startpoint;
+    std::chrono::time_point<std::chrono::high_resolution_clock> endpoint;
+    std::chrono::duration<long long, std::nano>                 duration;
+    
+    // constructor
+    Timer()         {startpoint = std::chrono::high_resolution_clock::now();}
+    void start()    {startpoint = std::chrono::high_resolution_clock::now();}
+    void stop()     {endpoint   = std::chrono::high_resolution_clock::now(); fetchtime();}
+    
+    void fetchtime(){
+        duration = std::chrono::duration_cast<std::chrono::nanoseconds>(endpoint - startpoint);
+        cout << format("{}  nanoseconds \n", duration.count());
+    }
+    void fetchtime(string stringarg){
+        duration = std::chrono::duration_cast<std::chrono::nanoseconds>(endpoint - startpoint);
+        cout << format("{} took {}  nanoseconds \n", stringarg, duration.count());
+    }
+    void measure(){
+        auto temp = std::chrono::high_resolution_clock::now();
+        auto nsduration  = std::chrono::duration_cast<std::chrono::nanoseconds>(temp - startpoint);
+        auto msduration  = std::chrono::duration_cast<std::chrono::microseconds>(temp - startpoint);
+        auto sduration   = std::chrono::duration_cast<std::chrono::seconds>(temp - startpoint);
+        cout << format("{} nanoseconds | {} microseconds | {} seconds \n", 
+            nsduration.count(), msduration.count(), sduration.count());
+    }
+    ~Timer(){
+        measure();
+    }
+};
+
+// main-file ===================================================================
+int main(){
+
+    // starting timer
+    Timer timer;
+    
+    // input- configuration
+    auto head0  {new ListNode(1)};
+    head0->next = new ListNode(4);
+    head0->next->next = new ListNode(5);
+
+    auto head1  {new ListNode(1)};
+    head1->next = new ListNode(3);
+    head1->next->next = new ListNode(4);
+
+    auto head2  {new ListNode(2)};
+    head2->next = new ListNode(6);
+
+    vector<ListNode*> lists {head0, head1, head2};
+    
+
+    // setup
+    ListNode*           prehead = new ListNode();
+    ListNode*           traveller       {prehead};
+    bool                runcondition    {true};
+    vector<ListNode*>   travellerList; 
+    int                 smallestindex   {-1};
+    int                 smallestvalue   {std::numeric_limits<int>::max()};
+    
+    // filling up the traveller lists
+    for(int i = 0; i<lists.size(); ++i) {travellerList.push_back(lists[i]);}
+    
+    // running the loop
+    int counter {0};
+    while(runcondition){
+        
+        // going through the values
+        for(int i = 0; i < travellerList.size(); ++i){
+            if (travellerList[i]        !=  nullptr && 
+                travellerList[i]->val   <   smallestvalue) {
+                smallestvalue = travellerList[i]->val; 
+                smallestindex = i;
+            }
+        }
+        
+        // now that we have the smallest value and smallest index, we add to the prehead
+        if (smallestindex == -1) {break;}
+        traveller->next                 = travellerList[smallestindex];
+        traveller                       = traveller->next;
+        travellerList[smallestindex]    = travellerList[smallestindex]->next;
+        
+        // resetting
+        smallestvalue = std::numeric_limits<int>::max();
+        smallestindex = -1;
+    }
+
+    // printing the final-linked list
+    fPrintLinkedList("final-output = ", prehead->next);
+
+    // return
+    return(0);
+    
+}
