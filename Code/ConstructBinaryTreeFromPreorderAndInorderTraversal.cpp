34cee7ef015373df8986a02f855ca917a70c5fca
vrsreeganesh <vrsreeganesh@gmail.com>
2025-09-07 09:55:01 -0400

    Added: 105. Construct Binary Tree from Preorder and Inorder Traversal

---
+++ Code/ConstructBinaryTreeFromPreorderAndInorderTraversal.cpp
@@ -0,0 +1,207 @@
+// including header-files
+#include <algorithm>
+#include <unordered_set>
+#include <bitset>
+#include <climits>
+#include <cstddef>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <new>
+#include <stdlib.h>
+#include <unordered_map>
+#include <vector>
+#include <set>
+#include <numeric>
+#include <functional>
+
+
+// hash-deinfes
+#define PRINTSPACE  std::cout << "\n\n\n\n" << std::endl;
+#define PRINTLINE   std::cout << "==============================================" << std::endl;
+
+// borrowing from namespace std
+using std::cout;
+using std::endl;
+using std::vector;
+using std::string;
+using std::unordered_map;
+using std::map;
+using std::format;
+using std::deque;
+using std::pair;
+using std::min;
+using std::max;
+
+// vector printing function
+template<typename T>
+void fPrintVector(vector<T> input){
+    for(auto x: input) cout << x << ",";
+    cout << endl;
+}
+
+template<typename T>
+void fpv(vector<T> input){
+    for(auto x: input) cout << x << ",";
+    cout << endl;
+}
+
+template<typename T>
+void fPrintMatrix(vector<T> input){
+    for(auto x: input){
+        for(auto y: x){
+            cout << y << ",";
+        }
+        cout << endl;
+    }
+}
+
+template<typename T, typename T1>
+void fPrintHashmap(unordered_map<T, T1> input){
+    for(auto x: input){
+        cout << format("[{},{}] | ", x.first, x.second);
+    }
+    cout <<endl;
+}
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+void fPrintBinaryTree(TreeNode* root){
+    // sending it back
+    if (root == nullptr) return;
+    
+    // printing
+    PRINTLINE
+    cout << "root->val = " << root->val << endl;
+    
+    // calling the children
+    fPrintBinaryTree(root->left);
+    fPrintBinaryTree(root->right);
+    
+    // returning
+    return;
+    
+}
+
+void fPrintLinkedList(string    prefix,
+                      ListNode* root){
+    if (root == nullptr) return;
+    cout << prefix;
+    std::function<void(ListNode*)> runlinkedlist = [&runlinkedlist](ListNode* root){
+        if (root == nullptr) return;
+        cout << root->val << " -> ";
+        runlinkedlist(root->next);
+    };
+    runlinkedlist(root);
+    cout << "|" << endl;
+    return;
+}
+
+template<typename T>
+void fPrintContainer(T input){
+    for(auto x: input) cout << x << ", ";
+    cout << endl;
+    return;
+}
+
+struct Timer
+{
+    std::chrono::time_point<std::chrono::high_resolution_clock> startpoint;
+    std::chrono::time_point<std::chrono::high_resolution_clock> endpoint;
+    std::chrono::duration<long long, std::nano>                 duration;
+    
+    // constructor
+    Timer()         {startpoint = std::chrono::high_resolution_clock::now();}
+    void start()    {startpoint = std::chrono::high_resolution_clock::now();}
+    void stop()     {endpoint   = std::chrono::high_resolution_clock::now(); fetchtime();}
+    
+    void fetchtime(){
+        duration = std::chrono::duration_cast<std::chrono::nanoseconds>(endpoint - startpoint);
+        cout << format("{}  nanoseconds \n", duration.count());
+    }
+    void fetchtime(string stringarg){
+        duration = std::chrono::duration_cast<std::chrono::nanoseconds>(endpoint - startpoint);
+        cout << format("{} took {}  nanoseconds \n", stringarg, duration.count());
+    }
+    void measure(){
+        auto temp = std::chrono::high_resolution_clock::now();
+        auto nsduration  = std::chrono::duration_cast<std::chrono::nanoseconds>(temp - startpoint);
+        auto msduration  = std::chrono::duration_cast<std::chrono::microseconds>(temp - startpoint);
+        auto sduration   = std::chrono::duration_cast<std::chrono::seconds>(temp - startpoint);
+        cout << format("{} nanoseconds | {} microseconds | {} seconds \n", 
+            nsduration.count(), msduration.count(), sduration.count());
+    }
+    ~Timer(){
+        measure();
+    }
+};
+
+// main-file ===================================================================
+TreeNode* foo(vector<int> preorder, vector<int> inorder)
+{
+
+    // returning for trivial case
+    if (preorder.size() == 0 || inorder.size() == 0)   {return static_cast<TreeNode*>(nullptr);}
+
+    // setup
+    auto& rootvalue     {preorder[0]};
+    auto root           {new TreeNode(preorder[0])};
+
+    // sending it back for trivial case
+    if (preorder.size() == 1 && inorder.size() == 1)    {return root;}
+
+    // finding root-position
+    auto rootposition   = std::find(inorder.begin(),
+                                    inorder.end(),
+                                    rootvalue);
+
+    // creating left-subtree
+    auto leftinorder   {vector<int>(inorder.begin(), rootposition)};
+    auto rightinorder  {vector<int>(rootposition+1, inorder.end())};
+    
+    // creating subset of preorder
+    auto leftpreorder   {vector<int>(preorder.begin()+1, 
+                                     preorder.begin() + leftinorder.size() + 1)};
+    auto rightpreorder  {vector<int>(preorder.begin() + leftinorder.size()+1,
+                                     preorder.end())};
+    
+    // calling the function on left and right
+    root->left      = foo(leftpreorder,     leftinorder);
+    root->right     = foo(rightpreorder,    rightinorder);
+    
+    // returning the 
+    return root;
+
+}
+int main(){
+
+    // starting timer
+    Timer timer;
+    
+    // input- configuration
+    auto preorder   {vector<int>{3,9,20,15,7}};
+    auto inorder    {vector<int>{9,3,15,20,7}};
+    
+    // manually building the tree
+    auto root   {foo(preorder, inorder)};
+
+    // return
+    return(0);
+    
+}
