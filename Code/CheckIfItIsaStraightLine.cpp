24f14d3fa8a14c4862d3674d1c96904ca3caf9e5
vrsreeganesh <vrsreeganesh@gmail.com>
2025-09-15 09:03:37 -0400

    Added: 1232. Check If It Is a Straight Line

---
+++ Code/CheckIfItIsaStraightLine.cpp
@@ -0,0 +1,202 @@
+// including header-files
+#include <algorithm>
+#include <unordered_set>
+#include <bitset>
+#include <climits>
+#include <cstddef>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <new>
+#include <stdlib.h>
+#include <unordered_map>
+#include <vector>
+#include <set>
+#include <numeric>
+#include <functional>
+
+
+// hash-deinfes
+#define PRINTSPACE  std::cout << "\n\n\n\n" << std::endl;
+#define PRINTLINE   std::cout << "==============================================" << std::endl;
+
+// borrowing from namespace std
+using std::cout;
+using std::endl;
+using std::vector;
+using std::string;
+using std::unordered_map;
+using std::map;
+using std::format;
+using std::deque;
+using std::pair;
+using std::min;
+using std::max;
+
+// vector printing function
+template<typename T>
+void fPrintVector(vector<T> input){
+    for(auto x: input) cout << x << ",";
+    cout << endl;
+}
+
+template<typename T>
+void fpv(vector<T> input){
+    for(auto x: input) cout << x << ",";
+    cout << endl;
+}
+
+template<typename T>
+void fPrintMatrix(vector<T> input){
+    for(auto x: input){
+        for(auto y: x){
+            cout << y << ",";
+        }
+        cout << endl;
+    }
+}
+
+template<typename T, typename T1>
+void fPrintHashmap(unordered_map<T, T1> input){
+    for(auto x: input){
+        cout << format("[{},{}] | ", x.first, x.second);
+    }
+    cout <<endl;
+}
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+void fPrintBinaryTree(TreeNode* root){
+    // sending it back
+    if (root == nullptr) return;
+    
+    // printing
+    PRINTLINE
+    cout << "root->val = " << root->val << endl;
+    
+    // calling the children
+    fPrintBinaryTree(root->left);
+    fPrintBinaryTree(root->right);
+    
+    // returning
+    return;
+    
+}
+
+void fPrintLinkedList(string    prefix,
+                      ListNode* root){
+    if (root == nullptr) return;
+    cout << prefix;
+    std::function<void(ListNode*)> runlinkedlist = [&runlinkedlist](ListNode* root){
+        if (root == nullptr) return;
+        cout << root->val << " -> ";
+        runlinkedlist(root->next);
+    };
+    runlinkedlist(root);
+    cout << "|" << endl;
+    return;
+}
+
+template<typename T>
+void fPrintContainer(T input){
+    for(auto x: input) cout << x << ", ";
+    cout << endl;
+    return;
+}
+
+struct Timer
+{
+    std::chrono::time_point<std::chrono::high_resolution_clock> startpoint;
+    std::chrono::time_point<std::chrono::high_resolution_clock> endpoint;
+    std::chrono::duration<long long, std::nano>                 duration;
+    
+    // constructor
+    Timer()         {startpoint = std::chrono::high_resolution_clock::now();}
+    void start()    {startpoint = std::chrono::high_resolution_clock::now();}
+    void stop()     {endpoint   = std::chrono::high_resolution_clock::now(); fetchtime();}
+    
+    void fetchtime(){
+        duration = std::chrono::duration_cast<std::chrono::nanoseconds>(endpoint - startpoint);
+        cout << format("{}  nanoseconds \n", duration.count());
+    }
+    void fetchtime(string stringarg){
+        duration = std::chrono::duration_cast<std::chrono::nanoseconds>(endpoint - startpoint);
+        cout << format("{} took {}  nanoseconds \n", stringarg, duration.count());
+    }
+    void measure(){
+        auto temp = std::chrono::high_resolution_clock::now();
+        auto nsduration  = std::chrono::duration_cast<std::chrono::nanoseconds>(temp - startpoint);
+        auto msduration  = std::chrono::duration_cast<std::chrono::microseconds>(temp - startpoint);
+        auto sduration   = std::chrono::duration_cast<std::chrono::seconds>(temp - startpoint);
+        cout << format("{} nanoseconds | {} microseconds | {} seconds \n", 
+            nsduration.count(), msduration.count(), sduration.count());
+    }
+    ~Timer(){
+        measure();
+    }
+};
+
+// main-file ===================================================================
+int main(){
+
+    // starting timer
+    Timer timer;
+    
+    // input- configuration
+    vector<vector<int>> coordinates {
+        {1,2},
+        {2,3},
+        {3,4},
+        {4,5},
+        {5,6},
+        {6,7}
+    };
+    
+    // setup
+    vector<vector<int>> geometric_vectors(coordinates.size()-1);
+    std::transform(coordinates.begin()+1,
+                   coordinates.end(),
+                   geometric_vectors.begin(),
+                   [&coordinates](auto argx){return vector<int>{argx[0] - coordinates[0][0], argx[1] - coordinates[0][1]};}
+                    );
+
+    // calculating slope
+    vector<double> geometric_slopes(geometric_vectors.size());
+    std::transform(geometric_vectors.begin(),
+                   geometric_vectors.end(),
+                   geometric_slopes.begin(),
+                   [](auto argx){
+                    if (argx[0] == 0)   {return std::numeric_limits<double>::max();}
+                    else                {return static_cast<double>(argx[1])/static_cast<double>(argx[0]);}
+                   });
+
+    // checking if all elements are same 
+    auto areallsame = std::adjacent_find(geometric_slopes.begin(),
+                                         geometric_slopes.end(),
+                                         std::not_equal_to<>()) == geometric_slopes.end();
+
+    cout << format("coordinates = {}\n", coordinates);
+    cout << format("vectors     = {}\n", geometric_vectors);
+    cout << format("geometric_slops = {}\n", geometric_slopes);
+    cout << format("final-output = {}\n", areallsame);
+
+    // return
+    return(0);
+    
+}
