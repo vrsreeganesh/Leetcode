5f33ec20bf88d56d3676d3d957b7796bf548b27d
vrsreeganesh <vrsreeganesh@gmail.com>
2025-09-21 09:04:17 -0400

    Added: 988. Smallest String Starting From Leaf

---
+++ Code/SmallestStringStartingFromLeaf.cpp
@@ -0,0 +1,234 @@
+// including header-files
+#include <algorithm>
+#include <unordered_set>
+#include <bitset>
+#include <climits>
+#include <cstddef>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <new>
+#include <stdlib.h>
+#include <unordered_map>
+#include <vector>
+#include <set>
+#include <numeric>
+#include <functional>
+
+
+// hash-deinfes
+#define PRINTSPACE  std::cout << "\n\n\n\n" << std::endl;
+#define PRINTLINE   std::cout << "==============================================" << std::endl;
+
+// borrowing from namespace std
+using std::cout;
+using std::endl;
+using std::vector;
+using std::string;
+using std::unordered_map;
+using std::map;
+using std::format;
+using std::deque;
+using std::pair;
+using std::min;
+using std::max;
+
+// vector printing function
+template<typename T>
+void fPrintVector(vector<T> input){
+    for(auto x: input) cout << x << ",";
+    cout << endl;
+}
+
+template<typename T>
+void fpv(vector<T> input){
+    for(auto x: input) cout << x << ",";
+    cout << endl;
+}
+
+template<typename T>
+void fPrintMatrix(vector<T> input){
+    for(auto x: input){
+        for(auto y: x){
+            cout << y << ",";
+        }
+        cout << endl;
+    }
+}
+
+template<typename T, typename T1>
+void fPrintHashmap(unordered_map<T, T1> input){
+    for(auto x: input){
+        cout << format("[{},{}] | ", x.first, x.second);
+    }
+    cout <<endl;
+}
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+void fPrintBinaryTree(TreeNode* root){
+    // sending it back
+    if (root == nullptr) return;
+    
+    // printing
+    PRINTLINE
+    cout << "root->val = " << root->val << endl;
+    
+    // calling the children
+    fPrintBinaryTree(root->left);
+    fPrintBinaryTree(root->right);
+    
+    // returning
+    return;
+    
+}
+
+void fPrintLinkedList(string    prefix,
+                      ListNode* root){
+    if (root == nullptr) return;
+    cout << prefix;
+    std::function<void(ListNode*)> runlinkedlist = [&runlinkedlist](ListNode* root){
+        if (root == nullptr) return;
+        cout << root->val << " -> ";
+        runlinkedlist(root->next);
+    };
+    runlinkedlist(root);
+    cout << "|" << endl;
+    return;
+}
+
+template<typename T>
+void fPrintContainer(T input){
+    for(auto x: input) cout << x << ", ";
+    cout << endl;
+    return;
+}
+
+struct Timer
+{
+    std::chrono::time_point<std::chrono::high_resolution_clock> startpoint;
+    std::chrono::time_point<std::chrono::high_resolution_clock> endpoint;
+    std::chrono::duration<long long, std::nano>                 duration;
+    
+    // constructor
+    Timer()         {startpoint = std::chrono::high_resolution_clock::now();}
+    void start()    {startpoint = std::chrono::high_resolution_clock::now();}
+    void stop()     {endpoint   = std::chrono::high_resolution_clock::now(); fetchtime();}
+    
+    void fetchtime(){
+        duration = std::chrono::duration_cast<std::chrono::nanoseconds>(endpoint - startpoint);
+        cout << format("{}  nanoseconds \n", duration.count());
+    }
+    void fetchtime(string stringarg){
+        duration = std::chrono::duration_cast<std::chrono::nanoseconds>(endpoint - startpoint);
+        cout << format("{} took {}  nanoseconds \n", stringarg, duration.count());
+    }
+    void measure(){
+        auto temp = std::chrono::high_resolution_clock::now();
+        auto nsduration  = std::chrono::duration_cast<std::chrono::nanoseconds>(temp - startpoint);
+        auto msduration  = std::chrono::duration_cast<std::chrono::microseconds>(temp - startpoint);
+        auto sduration   = std::chrono::duration_cast<std::chrono::seconds>(temp - startpoint);
+        cout << format("{} nanoseconds | {} microseconds | {} seconds \n", 
+            nsduration.count(), msduration.count(), sduration.count());
+    }
+    ~Timer(){
+        measure();
+    }
+};
+
+// main-file ===================================================================
+string fCompareStrings(const string& runningstring, 
+                       const string& finaloutput)
+{
+    
+    // copying values
+    auto a  {runningstring};
+    auto b  {finaloutput};
+    
+    // init
+    if (finaloutput.size() == 0)    {return runningstring;}
+    
+    // going through characters one after the other
+    auto numminchars    = std::min(a.size(), b.size());
+
+    for(int i = 0; i<numminchars; ++i){
+        if (a[i] < b[i])            {return a;}
+        else if (a[i] > b[i])       {return b;}
+        else                        {continue;}
+    }
+    
+    // if we're here, it means that the first minchars are the same
+    if (a.size() < b.size())        {return a;}
+    else                            {return b;}
+    
+}
+void foo(TreeNode*  root,
+         string     runningstring,
+         string&    finaloutput){
+    
+    // sending it back
+    if (root == nullptr) return;
+    
+    // adding current character to string 
+    runningstring = std::string(1, static_cast<char>(root->val + 'a')) + runningstring;
+    
+    // calling the function on the two children
+    if (root->left == nullptr && root->right == nullptr){
+        finaloutput = fCompareStrings(runningstring, finaloutput);
+    }
+    else{
+        // calling the children
+        foo(root->left,     runningstring, finaloutput);
+        foo(root->right,    runningstring, finaloutput);
+    }
+    
+    // returning
+    return;
+}
+
+int main(){
+
+    // starting timer
+    Timer timer;
+    
+    // input- configuration
+    auto root           {new TreeNode(0)};
+    root->left          = new TreeNode(1);
+    root->right         = new TreeNode(2);
+
+    root->left->left    = new TreeNode(3);
+    root->left->right   = new TreeNode(4);
+
+    root->right->left   = new TreeNode(3);
+    root->right->right  = new TreeNode(4);
+    
+
+    // setup
+    string finaloutput = "";
+    
+    // calling the function
+    foo(root, "", finaloutput);
+
+    // returning finaloutput
+    cout << format("final-output = {}\n", finaloutput);
+
+    // return
+    return(0);
+    
+}
