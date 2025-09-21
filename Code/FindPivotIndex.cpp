ee2e3a588aa8658b7871cbaa54f8d0caf0759bdc
vrsreeganesh <vrsreeganesh@gmail.com>
2025-09-21 16:52:11 -0400

    Added: 724. Find Pivot Index

---
+++ Code/FindPivotIndex.cpp
@@ -0,0 +1,171 @@
+// including header-files
+#include <bitset>
+#include <climits>
+#include <cstddef>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <new>
+#include <stdlib.h>
+#include <unordered_map>
+#include <vector>
+#include <set>
+
+// hash-deinfes
+#define PRINTSPACE  std::cout << "\n\n\n\n" << std::endl;
+#define PRINTLINE   std::cout << "-----------------" << std::endl;
+
+// borrowing from namespace std
+using std::cout;
+using std::endl;
+using std::vector;
+using std::string;
+using std::unordered_map;
+using std::map;
+using std::format;
+using std::deque;
+using std::pair;
+
+// vector printing function
+template<typename T>
+void fPrintVector(vector<T> input){
+    for(auto x: input) cout << x << ",";
+    cout << endl;
+}
+
+template<typename T>
+void fPrintMatrix(vector<T> input){
+    for(auto x: input){
+        for(auto y: x){
+            cout << y << ",";
+        }
+        cout << endl;
+    }
+}
+
+template<typename T, typename T1>
+void fPrintHashmap(unordered_map<T, T1> input){
+    for(auto x: input){
+        cout << format("[{},{}] | ", x.first, x.second);
+    }
+    cout <<endl;
+}
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+void fPrintBinaryTree(TreeNode* root){
+    // sending it back
+    if (root == nullptr) return;
+    
+    // printing
+    PRINTLINE
+    cout << "root->val = " << root->val << endl;
+    
+    // calling the children
+    fPrintBinaryTree(root->left);
+    fPrintBinaryTree(root->right);
+    
+    // returning
+    return;
+    
+}
+
+void fPrintLinkedList(ListNode* root){
+    if (root == nullptr) return;
+    cout << root->val << ", ";
+    fPrintLinkedList(root);
+    return;
+}
+
+template<typename T>
+void fPrintContainer(T input){
+    for(auto x: input) cout << x << ", ";
+    cout << endl;
+    return;
+}
+
+struct StopWatch
+{
+    std::chrono::time_point<std::chrono::high_resolution_clock> startpoint;
+    std::chrono::time_point<std::chrono::high_resolution_clock> endpoint;
+    std::chrono::duration<long long, std::nano>                 duration;
+    
+    // constructor
+    StopWatch()     {startpoint = std::chrono::high_resolution_clock::now();}
+    void start()    {startpoint = std::chrono::high_resolution_clock::now();}
+    void stop()     {endpoint   = std::chrono::high_resolution_clock::now(); fetchtime();}
+    
+    void fetchtime(){
+        duration = std::chrono::duration_cast<std::chrono::nanoseconds>(endpoint - startpoint);
+        cout << format("{}  nanoseconds \n", duration.count());
+    }
+    void fetchtime(string stringarg){
+        duration = std::chrono::duration_cast<std::chrono::nanoseconds>(endpoint - startpoint);
+        cout << format("{} took {}  nanoseconds \n", stringarg, duration.count());
+    }
+};
+
+
+// main-file ===================================================================
+int main(){
+    
+    // input- configuration
+    vector<int> nums    {1, 7, 3, 6, 5, 6};
+
+    // setup
+    vector<int> numsleft(nums.size(), 0);
+    vector<int> numsright(nums.size(), 0);
+    int         sumleft     {0};
+    int         sumright    {0};
+    int         leftindex   {0};
+    int         rightindex  {0};
+
+    // runs 
+    for(int i = 0; i<nums.size(); ++i){
+
+        // fetching indices
+        leftindex   = i;
+        rightindex  = nums.size()-1-i;
+
+        // accumulating left and right values
+        sumleft     += nums[leftindex]; 
+        sumright    += nums[rightindex];
+
+        if (leftindex < nums.size()-1)    {numsleft[leftindex+1] = sumleft;}
+        if (rightindex > 0)            {numsright[rightindex-1] = sumright;}
+    }
+
+    // finding the mid-element
+    int finaloutput {-1};
+    for(int i = 0; i<nums.size(); ++i){
+        if(numsleft[i] == numsright[i]){
+            finaloutput = i; 
+            break;
+        }
+    }
+
+    // printing 
+    cout << format("numsleft = ");  fPrintVector(numsleft);
+    cout << format("numsright = "); fPrintVector(numsright);
+    cout << format("finaloutput = {}\n", finaloutput);
+    
+    // return
+    return(0);
+    
+}
