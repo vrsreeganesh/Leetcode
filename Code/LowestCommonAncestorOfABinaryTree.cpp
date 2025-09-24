a07bd854f25bd67c5583e089baf5114f34335f0c
vrsreeganesh <vrsreeganesh@gmail.com>
2025-09-24 15:42:33 -0400

    Added: 236. Lowest Common Ancestor of a Binary Tree

---
+++ Code/LowestCommonAncestorOfABinaryTree.cpp
@@ -0,0 +1,217 @@
+// including header-files
+#include <algorithm>
+#include <bitset>
+#include <climits>
+#include <cstddef>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <new>
+#include <stdlib.h>
+#include <unordered_map>
+#include <vector>
+#include <set>
+#include <numeric>
+#include <functional>
+
+
+// hash-deinfes
+#define PRINTSPACE  std::cout << "\n\n\n\n" << std::endl;
+#define PRINTLINE   std::cout << "==============================================" << std::endl;
+
+// borrowing from namespace std
+using std::cout;
+using std::endl;
+using std::vector;
+using std::string;
+using std::unordered_map;
+using std::map;
+using std::format;
+using std::deque;
+using std::pair;
+using std::min;
+using std::max;
+
+// vector printing function
+template<typename T>
+void fPrintVector(vector<T> input){
+    for(auto x: input) cout << x << ",";
+    cout << endl;
+}
+
+template<typename T>
+void fpv(vector<T> input){
+    for(auto x: input) cout << x << ",";
+    cout << endl;
+}
+
+template<typename T>
+void fPrintMatrix(vector<T> input){
+    for(auto x: input){
+        for(auto y: x){
+            cout << y << ",";
+        }
+        cout << endl;
+    }
+}
+
+template<typename T, typename T1>
+void fPrintHashmap(unordered_map<T, T1> input){
+    for(auto x: input){
+        cout << format("[{},{}] | ", x.first, x.second);
+    }
+    cout <<endl;
+}
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+void fPrintBinaryTree(TreeNode* root){
+    // sending it back
+    if (root == nullptr) return;
+    
+    // printing
+    PRINTLINE
+    cout << "root->val = " << root->val << endl;
+    
+    // calling the children
+    fPrintBinaryTree(root->left);
+    fPrintBinaryTree(root->right);
+    
+    // returning
+    return;
+    
+}
+
+void fPrintLinkedList(string    prefix,
+                      ListNode* root){
+    if (root == nullptr) return;
+    cout << prefix;
+    std::function<void(ListNode*)> runlinkedlist = [&runlinkedlist](ListNode* root){
+        if (root == nullptr) return;
+        cout << root->val << " -> ";
+        runlinkedlist(root->next);
+    };
+    runlinkedlist(root);
+    cout << "|" << endl;
+    return;
+}
+
+template<typename T>
+void fPrintContainer(T input){
+    for(auto x: input) cout << x << ", ";
+    cout << endl;
+    return;
+}
+
+struct Timer
+{
+    std::chrono::time_point<std::chrono::high_resolution_clock> startpoint;
+    std::chrono::time_point<std::chrono::high_resolution_clock> endpoint;
+    std::chrono::duration<long long, std::nano>                 duration;
+    
+    // constructor
+    Timer()         {startpoint = std::chrono::high_resolution_clock::now();}
+    void start()    {startpoint = std::chrono::high_resolution_clock::now();}
+    void stop()     {endpoint   = std::chrono::high_resolution_clock::now(); fetchtime();}
+    
+    void fetchtime(){
+        duration = std::chrono::duration_cast<std::chrono::nanoseconds>(endpoint - startpoint);
+        cout << format("{}  nanoseconds \n", duration.count());
+    }
+    void fetchtime(string stringarg){
+        duration = std::chrono::duration_cast<std::chrono::nanoseconds>(endpoint - startpoint);
+        cout << format("{} took {}  nanoseconds \n", stringarg, duration.count());
+    }
+    void measure(){
+        auto temp = std::chrono::high_resolution_clock::now();
+        auto nsduration  = std::chrono::duration_cast<std::chrono::nanoseconds>(temp - startpoint);
+        auto msduration  = std::chrono::duration_cast<std::chrono::microseconds>(temp - startpoint);
+        auto sduration   = std::chrono::duration_cast<std::chrono::seconds>(temp - startpoint);
+        cout << format("{} nanoseconds | {} microseconds | {} seconds \n", 
+            nsduration.count(), msduration.count(), sduration.count());
+    }
+    ~Timer(){
+        measure();
+    }
+};
+
+// main-file ===================================================================
+int main(){
+
+    // starting timer
+    Timer timer;
+    
+    // input- configuration
+    auto root   {new TreeNode(3)};
+
+    root->left = new TreeNode(5);
+    root->right = new TreeNode(1);
+
+    root->left->left = new TreeNode(6);
+    root->left->right = new TreeNode(2);
+
+    root->right->left = new TreeNode(0);
+    root->right->right = new TreeNode(8);
+
+    root->left->right->left = new TreeNode(7);
+    root->left->right->right = new TreeNode(4);
+
+    auto p  {root->left};
+    auto q  {root->right};
+    
+    // setup
+    TreeNode *finalanswer {static_cast<TreeNode*>(nullptr)};
+    std::function<int(TreeNode*)> foo = [&foo,
+                           &finalanswer,
+                           &p, &q](
+        TreeNode* root){
+
+        // sending it back
+        if (root== nullptr)                 {return 0;}
+        if (finalanswer != nullptr)         {return 0;}
+
+        // searching left
+        auto leftsum     = foo(root->left);
+        auto rightsum    = foo(root->right);
+
+        // summing up values 
+        auto sumvalues = leftsum + rightsum;
+        if (root == p || root == q)         {sumvalues+=1;}
+
+        // updating final answer
+        if (sumvalues == 2 && 
+            finalanswer == nullptr)         {finalanswer = root;}
+
+        // returning values
+        return sumvalues;
+
+    };
+
+    // running the function
+    foo(root);
+
+    // returning the final answer
+    cout << format("Ancestor-value = {}, Ancestor-address = {}\n", 
+        finalanswer->val, static_cast<void*>(finalanswer));
+    
+
+    // return
+    return(0);
+    
+}
