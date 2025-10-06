755f715a55efb0fecef09bcd5197319acb72c151
vrsreeganesh <vrsreeganesh@gmail.com>
2025-10-06 19:09:26 -0400

    Added: 1971. Find if Path Exists in Graph

---
+++ Code/FindifPathExistsinGraph.cpp
@@ -0,0 +1,222 @@
+// including header-files
+#include <algorithm>
+#include <unordered_set>
+#include <bitset>
+#include <climits>
+#include <cstddef>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <new>
+#include <stdlib.h>
+#include <unordered_map>
+#include <vector>
+#include <set>
+#include <numeric>
+#include <functional>
+
+
+// hash-deinfes
+#define PRINTSPACE  std::cout << "\n\n\n\n" << std::endl;
+#define PRINTLINE   std::cout << "==============================================" << std::endl;
+
+// borrowing from namespace std
+using std::cout;
+using std::endl;
+using std::vector;
+using std::string;
+using std::unordered_map;
+using std::map;
+using std::format;
+using std::deque;
+using std::pair;
+using std::min;
+using std::max;
+
+// vector printing function
+template<typename T>
+void fPrintVector(vector<T> input){
+    for(auto x: input) cout << x << ",";
+    cout << endl;
+}
+
+template<typename T>
+void fpv(vector<T> input){
+    for(auto x: input) cout << x << ",";
+    cout << endl;
+}
+
+template<typename T>
+void fPrintMatrix(vector<T> input){
+    for(auto x: input){
+        for(auto y: x){
+            cout << y << ",";
+        }
+        cout << endl;
+    }
+}
+
+template<typename T, typename T1>
+void fPrintHashmap(unordered_map<T, T1> input){
+    for(auto x: input){
+        cout << format("[{},{}] | ", x.first, x.second);
+    }
+    cout <<endl;
+}
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+void fPrintBinaryTree(TreeNode* root){
+    // sending it back
+    if (root == nullptr) return;
+    
+    // printing
+    PRINTLINE
+    cout << "root->val = " << root->val << endl;
+    
+    // calling the children
+    fPrintBinaryTree(root->left);
+    fPrintBinaryTree(root->right);
+    
+    // returning
+    return;
+    
+}
+
+void fPrintLinkedList(string    prefix,
+                      ListNode* root){
+    if (root == nullptr) return;
+    cout << prefix;
+    std::function<void(ListNode*)> runlinkedlist = [&runlinkedlist](ListNode* root){
+        if (root == nullptr) return;
+        cout << root->val << " -> ";
+        runlinkedlist(root->next);
+    };
+    runlinkedlist(root);
+    cout << "|" << endl;
+    return;
+}
+
+template<typename T>
+void fPrintContainer(T input){
+    for(auto x: input) cout << x << ", ";
+    cout << endl;
+    return;
+}
+
+struct Timer
+{
+    std::chrono::time_point<std::chrono::high_resolution_clock> startpoint;
+    std::chrono::time_point<std::chrono::high_resolution_clock> endpoint;
+    std::chrono::duration<long long, std::nano>                 duration;
+    
+    // constructor
+    Timer()         {startpoint = std::chrono::high_resolution_clock::now();}
+    void start()    {startpoint = std::chrono::high_resolution_clock::now();}
+    void stop()     {endpoint   = std::chrono::high_resolution_clock::now(); fetchtime();}
+    
+    void fetchtime(){
+        duration = std::chrono::duration_cast<std::chrono::nanoseconds>(endpoint - startpoint);
+        cout << format("{}  nanoseconds \n", duration.count());
+    }
+    void fetchtime(string stringarg){
+        duration = std::chrono::duration_cast<std::chrono::nanoseconds>(endpoint - startpoint);
+        cout << format("{} took {}  nanoseconds \n", stringarg, duration.count());
+    }
+    void measure(){
+        auto temp = std::chrono::high_resolution_clock::now();
+        auto nsduration  = std::chrono::duration_cast<std::chrono::nanoseconds>(temp - startpoint);
+        auto msduration  = std::chrono::duration_cast<std::chrono::microseconds>(temp - startpoint);
+        auto sduration   = std::chrono::duration_cast<std::chrono::seconds>(temp - startpoint);
+        cout << format("{} nanoseconds | {} microseconds | {} seconds \n", 
+            nsduration.count(), msduration.count(), sduration.count());
+    }
+    ~Timer(){
+        measure();
+    }
+};
+
+// main-file ===================================================================
+int main(){
+
+    // starting timer
+    Timer timer;
+    
+    // input- configuration
+    const   auto    n   {3}; 
+    // const   auto    edges   {std::vector<std::vector<int>>{
+    const   auto    edges   {std::vector<std::vector<int>>{
+        {0,1},
+        {1,2},
+        {2,0}
+    }}; 
+    const   auto    source      {0};
+    const   auto    destination {2};
+
+    // building hashmap
+    unordered_map<int, vector<int>> hmap;
+
+    // going through the inputs
+    for(const   auto&   x: edges){
+        // adding to hashmap
+        if (hmap.find(x[0]) == hmap.end())  hmap[x[0]] = std::vector<int>({x[1]});
+        else                                hmap[x[0]].push_back(x[1]);
+        if (hmap.find(x[1]) == hmap.end())  hmap[x[1]] = std::vector<int>({x[0]});
+        else                                hmap[x[1]].push_back(x[0]);
+    }
+
+    // setting up for bfs launch
+    auto    visited     {std::vector<bool>(n, false)};
+    auto    found       {false};
+    auto    pipe        {std::deque<int>()};
+    pipe.push_back(source);
+
+    // peforming bfs launch
+    while(pipe.size() != 0 && found == false){
+
+        // popping from the front
+        auto    front_object    {pipe.front()}; pipe.pop_front();
+
+        // printing
+        cout << format("curr = {} | destination  = {}\n", front_object, destination);
+
+        // checking if current point is destination and updating found
+        if (front_object == destination)    {found = true; break;}
+
+        // checking if the current point has been visited already. if yes,c ontinue
+        if (visited[front_object] == true)  {continue;}
+        else                                {visited[front_object] = true;}
+
+        // else add the neighbours to the pipe
+        for(const   auto&   x: hmap[front_object])
+            pipe.push_back(x);
+    }
+
+    // printing hashmap
+    PRINTLINE
+    cout << format("final-output = {}\n", found);
+
+    
+    
+    
+
+    // return
+    return(0);
+    
+}
