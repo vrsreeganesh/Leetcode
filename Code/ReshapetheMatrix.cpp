e069ea4c9b5b011db10c39314ca30ebcd5fbb15e
vrsreeganesh <vrsreeganesh@gmail.com>
2025-10-19 22:50:05 -0400

    Added: 566. Reshape the Matrix

---
+++ Code/ReshapetheMatrix.cpp
@@ -0,0 +1,198 @@
+// including header-files
+#include <algorithm>
+#include <unordered_set>
+#include <bitset>
+#include <climits>
+#include <cstddef>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <new>
+#include <stdlib.h>
+#include <unordered_map>
+#include <vector>
+#include <set>
+#include <numeric>
+#include <functional>
+#include <deque>
+
+
+// hash-deinfes
+#define PRINTSPACE  std::cout << "\n\n\n\n" << std::endl;
+#define PRINTLINE   std::cout << "==============================================" << std::endl;
+
+// borrowing from namespace std
+using std::cout;
+using std::endl;
+using std::vector;
+using std::string;
+using std::unordered_map;
+using std::map;
+using std::format;
+using std::deque;
+using std::pair;
+using std::min;
+using std::max;
+
+// vector printing function
+template<typename T>
+void fPrintVector(vector<T> input){
+    for(auto x: input) cout << x << ",";
+    cout << endl;
+}
+
+template<typename T>
+void fpv(vector<T> input){
+    for(auto x: input) cout << x << ",";
+    cout << endl;
+}
+
+template<typename T>
+void fPrintMatrix(vector<T> input){
+    for(auto x: input){
+        for(auto y: x){
+            cout << y << ",";
+        }
+        cout << endl;
+    }
+}
+
+template<typename T, typename T1>
+void fPrintHashmap(unordered_map<T, T1> input){
+    for(auto x: input){
+        cout << format("[{},{}] | ", x.first, x.second);
+    }
+    cout <<endl;
+}
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+void fPrintBinaryTree(TreeNode* root){
+    // sending it back
+    if (root == nullptr) return;
+    
+    // printing
+    PRINTLINE
+    cout << "root->val = " << root->val << endl;
+    
+    // calling the children
+    fPrintBinaryTree(root->left);
+    fPrintBinaryTree(root->right);
+    
+    // returning
+    return;
+    
+}
+
+void fPrintLinkedList(string    prefix,
+                      ListNode* root){
+    if (root == nullptr) return;
+    cout << prefix;
+    std::function<void(ListNode*)> runlinkedlist = [&runlinkedlist](ListNode* root){
+        if (root == nullptr) return;
+        cout << root->val << " -> ";
+        runlinkedlist(root->next);
+    };
+    runlinkedlist(root);
+    cout << "|" << endl;
+    return;
+}
+
+template<typename T>
+void fPrintContainer(T input){
+    for(auto x: input) cout << x << ", ";
+    cout << endl;
+    return;
+}
+
+struct Timer
+{
+    std::chrono::time_point<std::chrono::high_resolution_clock> startpoint;
+    std::chrono::time_point<std::chrono::high_resolution_clock> endpoint;
+    std::chrono::duration<long long, std::nano>                 duration;
+    
+    // constructor
+    Timer()         {startpoint = std::chrono::high_resolution_clock::now();}
+    void start()    {startpoint = std::chrono::high_resolution_clock::now();}
+    void stop()     {endpoint   = std::chrono::high_resolution_clock::now(); fetchtime();}
+    
+    void fetchtime(){
+        duration = std::chrono::duration_cast<std::chrono::nanoseconds>(endpoint - startpoint);
+        cout << format("{}  nanoseconds \n", duration.count());
+    }
+    void fetchtime(string stringarg){
+        duration = std::chrono::duration_cast<std::chrono::nanoseconds>(endpoint - startpoint);
+        cout << format("{} took {}  nanoseconds \n", stringarg, duration.count());
+    }
+    void measure(){
+        auto temp = std::chrono::high_resolution_clock::now();
+        auto nsduration  = std::chrono::duration_cast<std::chrono::nanoseconds>(temp - startpoint);
+        auto msduration  = std::chrono::duration_cast<std::chrono::microseconds>(temp - startpoint);
+        auto sduration   = std::chrono::duration_cast<std::chrono::seconds>(temp - startpoint);
+        cout << format("{} nanoseconds | {} microseconds | {} seconds \n", 
+            nsduration.count(), msduration.count(), sduration.count());
+    }
+    ~Timer(){
+        measure();
+    }
+};
+
+// main-file ===================================================================
+int main(){
+
+    // starting timer
+    Timer timer;
+    
+    // input- configuration
+    auto    mat     {std::vector<std::vector<int>>({
+        {1,2},
+        {3,4}
+    })};
+    auto    r       {1};
+    auto    c       {4};
+
+    // setup
+    auto    canvas      {std::vector<std::vector<int>>(
+        r,
+        std::vector<int>(c, 0)
+    )};
+
+    // lambda for converting tid to row and columns
+    auto    coordsToTID = [&mat](auto input_vector){
+        return input_vector[0] * mat[0].size() + input_vector[1];
+    };
+    auto    tidToCoords =   [&c](auto   tid){
+        return std::vector<int>({static_cast<int>(tid/c), static_cast<int>(tid%c)});
+    };
+
+    // going through the mat
+    for(auto row = 0; row < mat.size(); ++row){
+        for(auto col = 0; col < mat[0].size(); ++col){
+            auto target_coords  {tidToCoords(row*mat[0].size() + col)};
+            canvas[target_coords[0]][target_coords[1]] = mat[row][col];
+        }
+    }
+
+    // printing 
+    fPrintMatrix(canvas);
+
+    // return
+    return(0);
+    
+}
